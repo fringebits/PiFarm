00c864cf62ff4c0db97724c231788ebb7ac4d5c6
mikeg <mikeg@fringebits.com>
2021-05-07 15:15:56 +0100

    Working version tmp102 is working; provided it is the only device on the bus

--- pifarm/pifarm.py
+++ pifarm/pifarm.py
@@ -1,16 +1,16 @@
 
 import time
-import esc.bus
-import esc.sensor_tmp102
+import smbus
+from esc import sensor_tmp102
 
 def main():
-    bus = esc.Bus(1)
-    sen = esc.SensorTMP102(bus)
+    bus = smbus.SMBus(1)
+    sen = sensor_tmp102.SensorTMP102(bus)
 
-    #while True:
-    val = sen.readTemp()
-    print(val)
-    #time.sleep(1)
+    while True:
+        val = sen.readTemperature()
+        print(val)
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
